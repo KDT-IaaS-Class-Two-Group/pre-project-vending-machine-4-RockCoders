94d90e8208c6bdea81ff69862cb555d407539047
eonduck2 <whdtnxd@gmail.com>
2024-06-24 17:48:29 +0900

    :sparkles: 이종수: 기본 제약 조건들을 받아올 수 있는 기능 구현

--- src/DB/modules/DBMANAGER.js
+++ src/DB/modules/DBMANAGER.js
@@ -23,21 +23,80 @@ class BaseDataBaseManager {
     });
   }
 
+  static getDefaultColumnProperties() {
+    return {
+      primaryKey: false,
+      unique: false,
+      notNull: false,
+      autoIncrement: false,
+      defaultValue: null,
+      check: null,
+      foreignKey: {
+        referencedTable: null,
+        referencedColumn: null,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+    };
+  }
+
   /**
    * @eonduck2 24.06.21
    * * 인자로 받은 테이블 이름과 컬럼들로 테이블 구성
    * @param { string } tableName 생성시킬 테이블 이름
    * @param { object } columns 컬럼 이름과 타입을 정의한 객체
    */
-  createTable(tableName, columns) {
+  createTable(tableName, columns, columnProperties) {
     const columnsDefinition = Object.entries(columns)
-      .map(([name, type]) => `${name} ${type}`)
+      .map(([name, type]) => {
+        const {
+          notNull,
+          unique,
+          primaryKey,
+          autoIncrement,
+          defaultValue,
+          check,
+          foreignKey,
+        } = columnProperties[name] || {};
+
+        let columnDef = `${name} ${type}`;
+
+        if (notNull) {
+          columnDef += " NOT NULL";
+        }
+        if (unique) {
+          columnDef += " UNIQUE";
+        }
+        if (primaryKey) {
+          columnDef += " PRIMARY KEY";
+        }
+        if (autoIncrement) {
+          columnDef += " AUTOINCREMENT";
+        }
+        if (defaultValue !== null && defaultValue !== undefined) {
+          columnDef += ` DEFAULT ${defaultValue}`;
+        }
+        if (check) {
+          columnDef += ` CHECK (${check})`;
+        }
+        if (
+          foreignKey &&
+          foreignKey.referencedTable &&
+          foreignKey.referencedColumn
+        ) {
+          columnDef += ` REFERENCES ${foreignKey.referencedTable}(${foreignKey.referencedColumn})`;
+          columnDef += ` ON DELETE ${foreignKey.onDelete}`;
+          columnDef += ` ON UPDATE ${foreignKey.onUpdate}`;
+        }
+
+        return columnDef;
+      })
       .join(", ");
 
     const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${columnsDefinition})`;
     this.db.run(sql, (err) => {
       if (err) {
-        throw new Error(`테이블 생성 오류 (${tableName})`);
+        throw new Error(`테이블 생성 오류 (${tableName}): ${err.message}`);
       } else {
         console.log(`테이블 "${tableName}" 생성 완료`);
       }
