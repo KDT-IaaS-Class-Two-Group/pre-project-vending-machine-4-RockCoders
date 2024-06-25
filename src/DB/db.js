f0b8fc4a935e5e088d212700a8fd2232da03546f
eonduck2 <whdtnxd@gmail.com>
2024-06-25 12:37:39 +0900

    :package: 이종수: DB 모델 테스트 완료

--- src/DB/db.js
+++ src/DB/db.js
@@ -3,14 +3,27 @@ import DataBaseManager from "./modules/DBMANAGER.js";
 const test = new DataBaseManager(`123.db`);
 
 test.db.serialize(() => {
-  const constants = DataBaseManager.getDefaultColumnProperties();
+  //   const constants = DataBaseManager.getDefaultColumnProperties();
+  //   const idConstants = constants;
+  //   constants.primaryKey = true;
+  //   console.log(constants);
+  const idConstants = DataBaseManager.getDefaultConstants();
+  idConstants.type.INTEGER = true;
+  idConstants.primaryKey = true;
+  idConstants.autoIncrement = true;
 
+  const nameConstants = DataBaseManager.getDefaultConstants();
+  nameConstants.foreignKey.referencedTable = `test_tbl`;
+  nameConstants.foreignKey.referencedColumn = `name`;
+  console.log(DataBaseManager.someMethod(nameConstants));
+
+  // console.log(DataBaseManager.someMethod(idConstants));
   const columns = {
-    id: "INTEGER",
-    name: "TEXT",
+    id: DataBaseManager.someMethod(idConstants),
+    name: `TEXT ${DataBaseManager.someMethod(nameConstants)}`,
     age: "INTEGER",
     city: "TEXT",
   };
 
-  test.createTable(`test_tbl`, columns);
+  test.createTable(`test_tbl5`, columns);
 });
--- src/DB/modules/DBMANAGER.js
+++ src/DB/modules/DBMANAGER.js
@@ -23,13 +23,15 @@ class BaseDataBaseManager {
     });
   }
 
-  /**
-   * @eonduck2 24.06.24
-   * * 테이블의 기본 제약 조건들을 리턴해주는 기능
-   * @returns { object } 기본 제약 조건 속성들이 정의된 객체
-   */
-  static getDefaultColumnProperties() {
+  static getDefaultConstants() {
     return {
+      type: {
+        NULL: false,
+        INTEGER: false,
+        REAL: false,
+        TEXT: false,
+        BLOB: false,
+      },
       primaryKey: false,
       unique: false,
       notNull: false,
@@ -43,6 +45,44 @@ class BaseDataBaseManager {
         onUpdate: "CASCADE",
       },
     };
+  }
+
+  // /**
+  //  * @eonduck2 24.06.24
+  //  * * 테이블의 기본 제약 조건들을 리턴해주는 기능
+  //  * @returns { object } 기본 제약 조건 속성들이 정의된 객체
+  //  */
+
+  static someMethod(properties) {
+    const constraints = [];
+
+    for (const [type, isSet] of Object.entries(properties.type)) {
+      if (isSet) {
+        constraints.push(type);
+        break; // 한 번에 하나의 타입만 추가
+      }
+    }
+    if (properties.primaryKey) constraints.push("PRIMARY KEY");
+    if (properties.unique) constraints.push("UNIQUE");
+    if (properties.notNull) constraints.push("NOT NULL");
+    if (properties.autoIncrement) constraints.push("AUTOINCREMENT");
+    if (properties.defaultValue !== null)
+      constraints.push(`DEFAULT ${properties.defaultValue}`);
+    if (properties.check !== null)
+      constraints.push(`CHECK(${properties.check})`);
+    if (
+      properties.foreignKey.referencedTable &&
+      properties.foreignKey.referencedColumn
+    ) {
+      constraints.push(
+        `REFERENCES ${properties.foreignKey.referencedTable}(${properties.foreignKey.referencedColumn})`
+      );
+      if (properties.foreignKey.onDelete)
+        constraints.push(`ON DELETE ${properties.foreignKey.onDelete}`);
+      if (properties.foreignKey.onUpdate)
+        constraints.push(`ON UPDATE ${properties.foreignKey.onUpdate}`);
+    }
+    return constraints.join(" ");
   }
 
   /**
