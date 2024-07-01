f452a2603a7c65284075525c3fe9f6a5897d38c7
jojayeon <jojayeon6152@gmail.com>
2024-07-01 21:46:25 +0900

    :package: 조자연 : 총액 계산 함수 다시만들기(테스트전)

--- src/client/ts/modules/selectProduct/totalPrice.ts
+++ src/client/ts/modules/selectProduct/totalPrice.ts
@@ -6,11 +6,12 @@
  */
 
 // 객체 들어오면 product로 받아서 그 객체의 price의 가격만 덧셈하는 방식
-export default((selectedProducts: { name: string, price: number }[]) => {
-  // 선택한 제품들을 기반으로 총액 계산 로직 구현
+// calculateTotalPrice.ts
+
+export default (selectedProducts: { id: number, name: string, price: number }[]): number => {
   let totalPrice = 0;
   selectedProducts.forEach(product => {
     totalPrice += product.price;
   });
   return totalPrice;
-})
+}
