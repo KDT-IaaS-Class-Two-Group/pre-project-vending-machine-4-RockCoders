e37b4e070b83672100030f52a63940ca88a48428
jojayeon <jojayeon6152@gmail.com>
2024-07-02 00:42:24 +0900

    조자연 : 테스트중

--- src/client/main.ts
+++ src/client/main.ts
@@ -6,6 +6,13 @@ import totalPriceAll from '../client/ts/modules/selectProduct/totalPriceAll.js';
 document.getElementById('money-button')?.addEventListener('click',priceInput);
 const localStorageModel = new LocalStorageModel();
 
-document.getElementById('container')?.addEventListener('click', () => {selectProductOutput()});
-document.getElementById('container')?.addEventListener('click', () => {totalPriceAll()});
+// document.getElementById('container1')?.addEventListener('click', () => {selectProductOutput()});
+// document.getElementById('container1')?.addEventListener('click', () => {totalPriceAll()});
 // container을 목찾음
+document.getElementById('container1')?.addEventListener('click', (event) => {
+  const target = event.target as HTMLElement;
+  if (target.id === 'container1') {
+    selectProductOutput();
+    totalPriceAll();
+  }
+});
