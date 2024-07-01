f55ce57bf8d4d897b72b200840368d9e55276d50
jojayeon <jojayeon6152@gmail.com>
2024-07-01 21:47:13 +0900

    :package: 조자연 : 총액 출력 함수 다시만들기(테스트전)

--- src/client/ts/modules/selectProduct/totalPriceOutput.ts
+++ src/client/ts/modules/selectProduct/totalPriceOutput.ts
@@ -3,18 +3,20 @@
  * * 총액 출력하는 함수
  * 저장해 놓은 총액 넘버를 가져와서 출력만 함
  * @param totalPrice - 총액 totalprice의 리턴값을 넣어준다. 또는 객체 변수 넣어주면됨  
- * @param paymentAmount - 로컬스토리지 balance 총액
  */
-export default ((totalPrice: number, paymentAmount: number) => {
-  const totalPriceElement = document.getElementById('total-price') as HTMLUListElement;
-  const messageElement = document.getElementById('message') as HTMLParagraphElement;
+
+// displayTotalPrice.ts
+
+export default function displayTotalPrice(totalPrice: number, paymentAmount: number): void {
+  const totalPriceElement = document.getElementById('total-price') as HTMLUListElement | null;
+  const messageElement = document.getElementById('message') as HTMLParagraphElement | null;
 
   if (totalPriceElement) {
     if (paymentAmount < totalPrice) {
       if (messageElement) {
         messageElement.innerText = '금액을 더 넣어 주세요';
       } else {
-        console.error('total-Price 없슈');
+        console.error('message 요소가 존재하지 않습니다.');
       }
     } else {
       totalPriceElement.innerText = `총액: ${totalPrice}원`;
@@ -23,6 +25,6 @@ export default ((totalPrice: number, paymentAmount: number) => {
       }
     }
   } else {
-    console.error('message 없슈');
+    console.error('total-price 요소가 존재하지 않습니다.');
   }
-});
+}
