9175804191846ab47bc2e9d8726333814c7987d9
jojayeon <jojayeon6152@gmail.com>
2024-07-01 21:43:41 +0900

    불러와서 이벤트만들기 중

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,11 @@
 import { priceInput } from "./ts/modules/amount/priceInput.js";
-import totalPriceOutput from "./ts/modules/selectProduct/totalPriceOutput.js";
-import totalPrice from "./ts/modules/selectProduct/totalPrice.js";
 import LocalStorageModel from "../localStorage/localStorage.js";
+import selectProductOutput from "../client/ts/modules/selectProduct/selectProductOutput.js"
+import totalPriceAll from '../client/ts/modules/selectProduct/totalPriceAll.js';
 
 document.getElementById('money-button')?.addEventListener('click',priceInput);
 const localStorageModel = new LocalStorageModel();
+
+document.getElementById('container')?.addEventListener('click', () => {selectProductOutput()});
+document.getElementById('container')?.addEventListener('click', () => {totalPriceAll()});
+// container을 목찾음
